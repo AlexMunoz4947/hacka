c7f6604fdd3d2acb8538f0846a252f7f4c81b39d
Alex4947 <alexander.munnoz12@gmail.com>
2024-08-31 17:46:51 -0500

    suma

---
+++ src/calculator.py
@@ -0,0 +1,41 @@
+class Calculadora:
+
+    @staticmethod
+    def sumar(a, b):
+        return a + b
+
+    @staticmethod
+    def restar(a, b):
+        return a - b
+
+    @staticmethod
+    def multiplicar(a, b):
+        return a * b
+
+    @staticmethod
+    def dividir(a, b):
+        if b == 0:
+            raise ValueError("No se puede dividir entre cero")
+        return a / b
+
+# Ejemplo de uso
+if __name__ == "__main__":
+    calculadora = Calculadora()
+    while True:
+        # Solicitar al usuario que ingrese la operación
+        entrada = input("Escriba la operación (por ejemplo, (4 + 4) * 2) o 'c' para borrar: ")
+
+        # Permitir al usuario borrar la operación
+        if entrada.lower() == 'c':
+            print("Operación borrada.")
+            continue
+
+        try:
+            # Evaluar la expresión completa utilizando eval
+            resultado = eval(entrada)
+            print(f"Resultado: {resultado}")
+
+        except ZeroDivisionError:
+            print("Error: No se puede dividir entre cero.")
+        except Exception as e:
+            print(f"Error: {e}. Por favor, ingrese una operación válida.")
